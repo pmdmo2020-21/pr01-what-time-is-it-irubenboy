d688ceafbe5d0299455da44bccfb08d6b62133c4
irubenboy <rubenmoreno.saladillo@gmail.com>
2020-09-28 16:23:46 +0200

    Creación de la aplicación
    Se ha creado la aplicación para que muestre la ficha y hora actual

--- app/src/main/java/es/iessaladillo/ruben/whattimeisit/MainActivity.java
+++ app/src/main/java/es/iessaladillo/ruben/whattimeisit/MainActivity.java
@@ -1,7 +1,12 @@
 package es.iessaladillo.ruben.whattimeisit;
 
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setupView();
+    }
+
+    private void setupView() {
+        String actualDate = dateNow();
+        String actualTime = timeNow();
+        TextView lblFecha = ActivityCompat.requireViewById(this, R.id.lblFecha );
+        TextView lblHora = ActivityCompat.requireViewById(this, R.id.lblHora);
+        lblFecha.setText(actualDate);
+        lblHora.setText(actualTime);
+    }
+
+    private String timeNow() {
+        return new SimpleDateFormat("HH:mm").format(new Date());
+    }
+
+    private String dateNow() {
+        Date now = new Date();
+        SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+        return format.format(now);
     }
 }
